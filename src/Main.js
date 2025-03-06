2e63570f9d7a53d26fcf2e6b01c6d4d8be87409b
rakaso598 <nexxus86@outlook.com>
2025-03-06 09:32:32 +0900

    init

---
+++ src/Main.js
@@ -0,0 +1,32 @@
+import App from "./components/App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishListPage from "./pages/WishlistPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import QuestionPage from "./pages/QuestionPage";
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />}></Route>
+            <Route
+              path="courses/react-frontend-development"
+              element={<CoursePage />}
+            ></Route>
+          </Route>
+          <Route path="wishlist" element={<WishListPage />} />
+          <Route path="questions" element={<QuestionListPage />} />
+          <Route path="questions/:id" element={<QuestionPage />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
